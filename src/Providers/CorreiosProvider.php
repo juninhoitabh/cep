3675ef847ec3ed66ecacace338bbc86183c6f01d
Junior Paz <juninhoitabh@gmail.com>
2020-06-21 13:43:36 -0300

    Correção provider correios, tratamento de erros

--- src/Providers/CorreiosProvider.php
+++ src/Providers/CorreiosProvider.php
@@ -10,7 +10,7 @@ use Symfony\Component\DomCrawler\Crawler;
 class CorreiosProvider implements ProviderContract
 {
     /**
-     * @return Address|null
+     * @return Address
      */
     public function getAddress($cep, HttpClientContract $client)
     {
@@ -18,19 +18,20 @@ class CorreiosProvider implements ProviderContract
             'CEP' => $cep,
         ]);
         
-        if(!is_null($response)) {
-            
-            $crawler = new Crawler($response);
-
-            $message = $crawler->filter('div.ctrlcontent p')->html();
-            
-            if($message == 'DADOS ENCONTRADOS COM SUCESSO.') 
+        if(!is_null($response)) 
+        {
+            try
             {
-                $tr = $crawler->filter('table.tmptabela');
+                $crawler = new Crawler($response);
 
-                $params['zipcode'] = $cep;
-                try
+                $message = $crawler->filter('div.ctrlcontent p')->html();
+            
+                if($message == 'DADOS ENCONTRADOS COM SUCESSO.') 
                 {
+                    $tr = $crawler->filter('table.tmptabela');
+
+                    $params['zipcode'] = $cep;
+                
                     $params['street'] = $tr->filter('tr:nth-child(1) td:nth-child(2)')->html();
                     $params['neighborhood'] = $tr->filter('tr:nth-child(2) td:nth-child(2)')->html();
 
@@ -42,24 +43,24 @@ class CorreiosProvider implements ProviderContract
                     $params['street'] = (count($aux) == 2) ? $aux[0] : $params['street'];
                     
                     $params['ibge'] = NULL;
-                } catch (Exception $e) {
-                    return NULL;           
-                }
                 
-                return Address::create(array_map(function ($item) {
-                    return urldecode(str_replace('%C2%A0', '', urlencode($item)));
-                }, $params));
-            }
-            elseif($message == 'CEP NAO ENCONTRADO' || $message == 'DADOS NAO ENCONTRADOS') 
-            {
-                return Address::create([
-                    'zipcode'      => $cep,
-                    'street'       => NULL,
-                    'neighborhood' => NULL,
-                    'city'         => NULL,
-                    'state'        => NULL,
-                    'ibge'         => NULL,
-                ]);
+                    return Address::create(array_map(function ($item) {
+                        return urldecode(str_replace('%C2%A0', '', urlencode($item)));
+                    }, $params));
+                }
+                elseif($message == 'CEP NAO ENCONTRADO' || $message == 'DADOS NAO ENCONTRADOS') 
+                {
+                    return Address::create([
+                        'zipcode'      => $cep,
+                        'street'       => NULL,
+                        'neighborhood' => NULL,
+                        'city'         => NULL,
+                        'state'        => NULL,
+                        'ibge'         => NULL,
+                    ]);
+                }
+            } catch (\InvalidArgumentException $e) {
+                return NULL;
             }
         }
     }
